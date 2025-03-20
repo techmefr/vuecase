36a704dfc212051019ec08a58b8998fc837e6758
techmefr <gaetan-compigni@wilder.school>
2025-03-20 14:47:30 +0100

    add img and fix import

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,7 +1,66 @@
-<script setup></script>
+<script setup>
+import SocialIcon from './SocialIcon.vue';
+</script>
 
 <template>
-    <img id="profile-img" src="../assets/me.jpg" alt="me" width="300px" height="300px"/>
+    <div id="top-line">
+        <img id="profile-img" src="../assets/me.jpg" alt="me" width="300px" height="300px" />
+        <div id="line">
+            <p>Je suis <span class="text-highlight-1">Anthony CARDINALE</span>, <br />
+                Ingénieur en informatique.</p>
+            <div id="social">
+                <SocialIcon img="yt" alt="youtube" link="https://www.youtube.com/@F0rmati0nFacile" />
+                <SocialIcon img="in" alt="linkedin"
+                    link="https://www.linkedin.com/in/anthony-cardinale-%F0%9F%91%A8%F0%9F%8F%BB%E2%80%8D%F0%9F%92%BB-39474243/" />
+                <SocialIcon img="ud" alt="udemy" link="https://www.udemy.com/user/cardinale-anthony/" />
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#top-line {
+    margin-top: 50px;
+    width: 100%;
+    height: 300px;
+}
+
+#top-line #profile-img {
+    float: left;
+    margin-right: 50px;
+}
+
+#top-line #line {
+    float: left;
+    transform: translateY(40%);
+    text-align: center;
+}
+
+#top-line p {
+    font-size: 42px;
+    line-height: 60px;
+}
+
+#top-line p span {
+    font-weight: bold;
+}
+
+#social {
+    margin-top: 15px;
+}
+
+@media screen and (max-width: 905px) {
+    #top-line #profile-img {
+        display: none;
+    }
+
+    #top-line #line {
+        float: none;
+        transform: translateY(0%);
+    }
+
+    #top-line p {
+        font-size: 36px;
+    }
+}
+</style>
---
+++ src/components/SocialIcon.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    img: String,
+    link: String,
+    alt: String
+})
+</script>
+
+<template>
+    <a :href="props.link" target="_blank">
+        <img class="social-img" :src="`/src/assets/${props.img}.png`" :alt="props.alt" width="32px" />
+    </a>
+</template>
+
+<style scoped>
+.social-img {
+    margin-right: 10px;
+    filter: opacity(0.8);
+}
+</style>
