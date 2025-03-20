a7bb87a47be84ee811e48c996e44107109591d40
techmefr <gaetan-compigni@wilder.school>
2025-03-20 13:50:54 +0100

    init project and firsts components

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <img id="profile-img" src="../assets/me.jpg" alt="me" width="300px" height="300px"/>
+</template>
+
+<style scoped></style>
---
+++ src/pages/PortfolioMain.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PageTitle from 'src/components/PageTitle.vue'
+</script>
+
+<template>
+    <div>
+        <PageTitle/>
+    </div>
+</template>
+
+<style scoped></style>
